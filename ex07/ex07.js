03486c57460fdc9a56f9bb0168a3ba32e70ecca1
JLucasM <menezes.joaolucas@gmail.com>
2022-04-07 16:48:01 -0300

    adicionando arquivos no repositório

---
+++ ex07/ex07.js
@@ -0,0 +1,20 @@
+document.getElementById("btn").addEventListener("click", () => {
+    const firstNumber = document.getElementById("firstNumber").value;
+    const secondNumber = document.getElementById("secondNumber").value;
+    document.getElementById(
+        "soma"
+    ).innerHTML = `${firstNumber} + ${secondNumber} = ${parseInt(firstNumber) + parseInt(secondNumber)
+        }`;
+    document.getElementById(
+        "menos"
+    ).innerHTML = `${firstNumber} - ${secondNumber} = ${parseInt(firstNumber) - parseInt(secondNumber)
+        }`;
+    document.getElementById(
+        "multi"
+    ).innerHTML = `${firstNumber} * ${secondNumber} = ${parseInt(firstNumber) * parseInt(secondNumber)
+        }`;
+    document.getElementById(
+        "pot"
+    ).innerHTML = `${firstNumber} ** ${secondNumber} = ${parseInt(firstNumber) ** parseInt(secondNumber)
+        }`;
+});
